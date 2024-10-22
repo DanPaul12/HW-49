a2f1e391f68673815a6163162b8cf27624c3c460
DanPaul12 <dan.paul.schechter@gmail.com>
2024-10-21 22:00:19 -0700

    hw49

--- react-props/src/movieslist.jsx
+++ react-props/src/movieslist.jsx
@@ -3,7 +3,10 @@ import React, { useState } from 'react';
 const MoviesList = () => {
     // Initialize state with a list of movies
     // Implement required functionalities here
-    const [movies, setMovies] = useState(['Gilbert Grape', 'Independence Day', 'Black Panther'])
+    const [movies, setMovies] = useState([{title: 'Gilbert Grape', details: 'Released June 1, 1993'},
+                                        {title: 'Independence Day', details: 'Released July 4, 1999'},
+                                        {title: 'Black Panther', details: 'Released January 23, 2021'},
+    ])
     const [showAbout, setShowAbout] = useState(true)
 
     return (
@@ -12,8 +15,8 @@ const MoviesList = () => {
             {/* Toggle view button */}
             <ul>
                 {/* Map through the movies and display them */}
-                {movies.map((movie)=> (
-                    <li>{movie} <button className='remove'>Remove</button> </li>
+                {movies.map((movie, index)=> (
+                    <li> {key = {index}> movie.title} <button className='remove'>Remove</button> </li>
                 ))}
             </ul>
         </div>
