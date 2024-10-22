b7c0da32b274885424c6fa10605b18a5d8bd595d
DanPaul12 <dan.paul.schechter@gmail.com>
2024-10-22 11:40:26 -0700

    hw49

--- react-props/src/movieslist.jsx
+++ react-props/src/movieslist.jsx
@@ -3,13 +3,17 @@ import React, { useState } from 'react';
 const MoviesList = () => {
     // Initialize state with a list of movies
     // Implement required functionalities here
-    const [movies, setMovies] = useState([{title: 'Gilbert Grape', details: 'Released June 1, 1993'},
-                                        {title: 'Independence Day', details: 'Released July 4, 1999'},
-                                        {title: 'Black Panther', details: 'Released January 23, 2021'},
+    const [movies, setMovies] = useState([{title: 'Gilbert Grape', details: 'Released June 1, 1993', showAbout: false, genre: 'sadboi'},
+                                        {title: 'Independence Day', details: 'Released July 4, 1999', showAbout: false, genre: 'action'},
+                                        {title: 'Black Panther', details: 'Released January 23, 2021', showAbout: false, genre: 'action'},
     ])
-    const [showAbout, setShowAbout] = useState(false)
-
-    const toggleAbout = () => setShowAbout(!showAbout)
+    const toggleAbout = (index1) => {
+        const setShowAbout = movies.map((movie, index2) => index1 === index2 ? {...movie, showAbout:!movie.showAbout}: movie)
+        setMovies(setShowAbout)
+    }
+    const removeMovie = (index1) => {
+        const filterMovie = movies.filter(movie => movie.index !== index1 )
+        setMovies(filterMovie)}
 
     return (
         <div>
@@ -18,7 +22,7 @@ const MoviesList = () => {
             <ul>
                 {/* Map through the movies and display them */}
                 {movies.map((movie, index)=> (
-                    <li key = {index} onClick={toggleAbout}> {movie.title} {showAbout && movie.details} <button className='remove'>Remove</button> </li>
+                    <li key = {index} onClick={() => toggleAbout(index)}> {movie.title} -- {movie.showAbout && movie.details} <button className='remove' onClick={() => removeMovie(index)}>Remove</button> </li>
                 ))}
             </ul>
         </div>
