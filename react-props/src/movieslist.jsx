9f399b187354f41982d9c3fc04518a2ab0e1451f
DanPaul12 <dan.paul.schechter@gmail.com>
2024-10-21 21:30:58 -0700

    hw49

--- react-props/src/movieslist.jsx
+++ react-props/src/movieslist.jsx
@@ -4,14 +4,16 @@ const MoviesList = () => {
     // Initialize state with a list of movies
     // Implement required functionalities here
     const [movies, setMovies] = useState(['Gilbert Grape', 'Independence Day', 'Black Panther'])
+    const [showAbout, setShowAbout] = useState(true)
 
     return (
         <div>
+            <h2>Movie List: </h2>
             {/* Toggle view button */}
             <ul>
                 {/* Map through the movies and display them */}
                 {movies.map((movie)=> (
-                    <li>{movie}</li>
+                    <li>{movie} <button className='remove'>Remove</button> </li>
                 ))}
             </ul>
         </div>
